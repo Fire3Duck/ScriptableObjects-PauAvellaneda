f0049ee821fcc7e8cee52e39a64a54d9082c92b2
Fire3Duck <pauavellanedalopez@gmail.com>
2026-04-02 17:19:39 +0200

    Okay

--- Assets/Scripts/LenguageManager.cs
+++ Assets/Scripts/LenguageManager.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
-using TMPro;
-public class LenguageManager : MonoBehaviour
+using UnityEngine.UI;
+    public class LenguageManager : MonoBehaviour
 {
- 
+    public ScriptableLanguage language;
+    public GameObject inspectWindow;
+
+    public Text inspectionName;
+    public Text inspectionDescription;
+    
+    public Button thisSlotButton;
+    public Button closeButton;
+    void Start()
+    {
+        thisSlotButton = GetComponentInChildren<Button>();
+        thisSlotButton.onClick.AddListener(InspectItem);
+    }
+
+    void InspectItem()
+    {
+        inspectWindow.SetActive(true);
+    }
+
+    void CloseWindow()
+    {
+        inspectWindow.SetActive(false);
+
+        closeButton.onClick.RemoveListener(CloseWindow);
+    }
 }
